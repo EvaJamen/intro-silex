25d0ffa70386d492875e75456cb00eaac02f8a4e
EvaJamen <eva.jamen@hetic.net>
2017-05-11 11:29:08 +0200

    route indiquant une valeur par défaut

--- web/index.php
+++ web/index.php
@@ -5,13 +5,24 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 // Init Silex
 $app = new Silex\Application();
-$app['debug'] = true;
+
+//détaille les erreurs
+$app['debug'] = true; //tableau associatif : $variable[tableau]
 
 // Create `hello` route
 $app->get('/hello', function()
 {
     return 'Hello!';
 });
+
+//Créer une route dynamiquement
+$app->get('/page/{number}', function ($number)
+{
+    return 'Page number : '.$number;
+})
+->assert('number', '\d+')
+->value('number', '1');
+
 
 // Run Silex
 $app->run();
